518f3acb49b1ef46e7753cf90767b85e1125a88c
Bilal Abouzid <imbilalabouzid@gmail.com>
2022-03-11 21:42:44 +0100

    build search users API + add token middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  loginUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
+
+router.get("/", protect, allUsers);
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
+
 module.exports = router;
