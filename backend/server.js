832862c839ae9d8a8082486ea261f7cc1d428dbb
Bilal Abouzid <imbilalabouzid@gmail.com>
2022-03-14 23:30:39 +0100

    apply realtime chating

--- backend/server.js
+++ backend/server.js
@@ -42,4 +42,46 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log("Server Started on Port: ", PORT));
+const server = app.listen(PORT, console.log("Server Started on Port: ", PORT));
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket");
+
+  socket.on("setup", (user) => {
+    socket.join(user._id);
+    console.log("connected:", user._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (chat) => {
+    socket.join(chat);
+    console.log("user joined chat:", chat);
+  });
+
+  socket.on("typing", (chat) => socket.in(chat).emit("typing"));
+  socket.on("stoptyping", (chat) => socket.in(chat).emit("stop typing"));
+  socket.on("new message", (newMessageRecived) => {
+    var chat = newMessageRecived.chat;
+
+    console.log(newMessageRecived);
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecived.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecived);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(user._id);
+  });
+});
